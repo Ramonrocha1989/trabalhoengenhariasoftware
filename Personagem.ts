b3ebab9ac1b13d1942b3f7c68ad90b8fae1dd8eb
Ramon Rocha <ramonrocha1989@gmail.com>
2020-11-10 21:45:24 -0300

    :rocket: Mudança de código do sansa

--- Personagem.ts
+++ Personagem.ts
@@ -31,6 +31,9 @@ while (option != 9) {
             break;
         case 3:
             console.log("sansa :>> ", sansa);
+            console.log("sansa :>> ", sansa);
+            console.log("sansa :>> ", sansa);
+            console.log("sansa :>> ", sansa);
             break;
         default:
             break;
